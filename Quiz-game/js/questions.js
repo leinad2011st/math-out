fd5dbdcf6e5e2bb313de133d7346a0ce0a88ad1e
leinad2011st <91507903+leinad2011st@users.noreply.github.com>
2021-09-28 07:30:57 -0300

    Add files via upload

---
+++ Quiz-game/js/questions.js
@@ -0,0 +1,163 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+    numb: 1,
+        question: "Every day, our python swallows two mice. How many mice do we need for three weeks?",
+    answer: "42",
+    options: [
+      "20",
+      "39",
+      "42",
+      "29"
+    ]
+    },
+    
+    {
+    numb: 2,
+    question: "In the month of September, 180 mice were swallowed by pythons in a zoo over the course of three weeks. How many pythons does the zoo have in total?",
+    answer: "3",
+    options: [
+      "5",
+      "3",
+      "8",
+      "7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Estimate:998+1344 to the nearest hundred",
+     answer: "2300",
+    options: [
+      "2300",
+      "1209",
+      "1203",
+      "1300"
+    ]
+  },
+    {
+    numb: 4,
+        question: "Whats 1098+1344",
+    answer: "2442",
+    options: [
+      "1237",
+      "2912",
+      "2142",
+      "2442"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what 9-4*2",
+    answer: "1",
+    options: [
+      "10",
+      "18",
+      "1",
+      "11"
+    ]
+    },
+{
+    numb: 6,
+        question: "whats 9-0*9",
+        answer: "9",
+        options: [
+         "0",
+         "87",
+         "81",
+         "9"
+        ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   //  {
+   //  numb: 6,
+   //  question: "Your Question is Here",
+   //  answer: "Correct answer of the question is here",
+   //  options: [
+   //    "Option 1",
+   //    "option 2",
+   //    "option 3",
+   //    "option 4"
+   //  ]
+   //},
+];
